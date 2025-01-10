2af3b34844092971b6d5f3e551e4c45a5bedffc1
malek lazreg <lazregmalek30@gmail.com>
2025-01-10 14:52:15 +0100

    counter

--- Week 1/day5/core/counter/server.py
+++ Week 1/day5/core/counter/server.py
@@ -3,7 +3,6 @@ from flask import Flask, session, render_template, redirect, request
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-# Root route to display the counter
 @app.route('/')
 def index():
     if 'counter' not in session:
@@ -11,13 +10,11 @@ def index():
     session['counter'] += 1
     return render_template('index.html', counter=session['counter'])
 
-# Route to destroy the session
 @app.route('/destroy_session')
 def destroy_session():
     session.clear()
     return redirect('/')
 
-# NINJA BONUS: Route to increment by 2
 @app.route('/increment_by_2')
 def increment_by_2():
     if 'counter' in session:
@@ -26,7 +23,6 @@ def increment_by_2():
         session['counter'] = 2
     return redirect('/')
 
-# SENSEI BONUS: Route to increment by a user-defined value
 @app.route('/increment', methods=['POST'])
 def increment():
     increment_value = int(request.form.get('increment', 1))
